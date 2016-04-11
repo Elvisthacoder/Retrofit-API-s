9cc77391cbb24b541890e4c884ae2c8f1c4500fa
Elvisthacoder <ekk213@gmail.com>
2016-04-11 15:01:13 +0300

    main

--- app/src/main/java/com/example/taifa/retrofit/MainActivity.java
+++ app/src/main/java/com/example/taifa/retrofit/MainActivity.java
@@ -3,11 +3,45 @@ package com.example.taifa.retrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+public class MainActivity extends ListActivity implements Callback<StackOverflowQuestions> {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+        ArrayAdapter<Question> arrayAdapter =
+                new ArrayAdapter<Question>(this,
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        new ArrayList<Question>());
+        setListAdapter(arrayAdapter);
+        setProgressBarIndeterminateVisibility(true);
+        setProgressBarVisibility(true);
     }
-}
