0ba5003aa3acb5b01e831f9b0d4d05f0735f5e72
Elvisthacoder <ekk213@gmail.com>
2016-04-11 15:06:19 +0300

    android

--- app/src/main/java/com/example/taifa/retrofit/MainActivity.java
+++ app/src/main/java/com/example/taifa/retrofit/MainActivity.java
@@ -57,3 +57,23 @@ public class MainActivity extends ListActivity implements Callback<StackOverflow
                 .baseUrl("https://api.stackexchange.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        // prepare call in Retrofit 2.0
+        StackOverflowAPI stackOverflowAPI = retrofit.create(StackOverflowAPI.class);
+
+        Call<StackOverflowQuestions> call = stackOverflowAPI.loadQuestions("android");
+        //asynchronous call
+        call.enqueue(this);
+
+        // synchronous call would be with execute, in this case you
+        // would have to perform this outside the main thread
+        // call.execute()
+
+        // to cancel a running request
+        // call.cancel();
+        // calls can only be used once but you can easily clone them
+        //Call<StackOverflowQuestions> c = call.clone();
+        //c.enqueue(this);
+
+        return true;
+    }
