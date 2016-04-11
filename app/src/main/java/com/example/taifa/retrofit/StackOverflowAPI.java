2cb68b5665fd801c95012923c192f5cd72487045
Elvisthacoder <ekk213@gmail.com>
2016-04-11 14:58:07 +0300

    API

--- app/src/main/java/com/example/taifa/retrofit/StackOverflowAPI.java
+++ app/src/main/java/com/example/taifa/retrofit/StackOverflowAPI.java
@@ -3,7 +3,15 @@ package com.example.taifa.retrofit;
 /**
  * Created by taifa on 4/11/16.
  */
-public class StackOverflowAPI {
 
-    
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+import retrofit.Call;
+//public class StackOverflowAPI {
+//}
+public interface StackOverflowAPI {
+//    @GET("/2.2/questions?order=desc&sort=creation&site=stackoverflow")
+    Call<StackOverflowQuestions> loadQuestions(@Query("tagged") String tags);
 }
