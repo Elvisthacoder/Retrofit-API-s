26ce69a3566c0427eade30157a4c99c884b5999a
Elvisthacoder <ekk213@gmail.com>
2016-04-11 14:49:01 +0300

    question

---
+++ app/src/main/java/com/example/taifa/retrofit/Question.java
@@ -0,0 +1,15 @@
+package com.example.taifa.retrofit;
+
+/**
+ * Created by taifa on 4/11/16.
+ */
+public class Question {
+
+    String title;
+    String link;
+
+    @Override
+    public String toString() {
+        return (title);
+    }
+}
